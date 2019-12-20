9cab74a97b1cd160ce2f1d4b5647af5670565883
Carlo Bernucci Gómez <carlobernucci@gmail.com>
2019-12-20 16:00:52 -0300

    initial app

---
+++ pages/categories.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div><h1>Categories</h1></div>
+        <div>
+      <a v-for="breed in breeds" :key="breed" class="breed">
+        {{breed}}
+      </a>
+    </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData () {
+    const { data } = await axios.get('https://api.jav4free.tv/jav4free/categories')
+    return { breeds: data.categories }
+  },
+}
+
+</script>
+
