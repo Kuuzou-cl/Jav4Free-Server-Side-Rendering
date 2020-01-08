427179fa74fc56072ebd1aa02c006b3981568006
devKuuzou <carlobernucci@gmail.com>
2020-01-08 20:55:43 -0300

    juicyads verification

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name:"juicyads-site-verification", content:"c23a8ba63c9c4356619c93eb2953dfaf" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'https://javdata.sfo2.cdn.digitaloceanspaces.com/favi-j4.webp' },
