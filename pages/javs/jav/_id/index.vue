a21890b783d03545b748a127522b896da3aa091e
devKuuzou <carlobernucci@gmail.com>
2020-02-09 21:08:48 -0300

    ads hot fix

--- pages/javs/jav/_id/index.vue
+++ pages/javs/jav/_id/index.vue
@@ -125,7 +125,7 @@
                 src="https://poweredby.jads.co/js/jads.js"
               ></script>
               <ins id="824390" data-width="300" data-height="262"></ins>
-              <script type="text/javascript" data-cfasync="false" async>
+              <script type="application/javascript" data-cfasync="false" async>
   (adsbyjuicy = window.adsbyjuicy || []).push({ adzone: 824390 });
               </script>
               <!--JuicyAds END-->
