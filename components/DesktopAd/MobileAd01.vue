00e660a79c6fb1321883da173cd22677f4b036f6
Kuuzou-cl <carlobernucci@gmail.com>
2020-07-04 05:06:59 -0400

    ads

--- components/DesktopAd/MobileAd01.vue
+++ components/DesktopAd/MobileAd01.vue
@@ -2,13 +2,13 @@
   <div>
     <!-- JuicyAds v3.1 -->
     <script
-      type="text/javascript"
+      type="application/javascript"
       data-cfasync="false"
       async
       src="https://poweredby.jads.co/js/jads.js"
     ></script>
     <ins id="856936" data-width="300" data-height="112"></ins>
-    <script type="text/javascript" data-cfasync="false" async>
+    <script type="application/javascript" data-cfasync="false" async>
   (adsbyjuicy = window.adsbyjuicy || []).push({ adzone: 856936 });
     </script>
     <!--JuicyAds END-->
