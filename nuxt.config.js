adc54e5695ff5d7fabff596dd18a4258f432dfab
Kuuzou-cl <carlobernucci@gmail.com>
2020-08-17 18:04:17 -0400

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,11 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      {
+        name: 'exoclick-site-verification',
+        content: '0922ef088413c0d106de848a80b5487f'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'https://javdata.sfo2.cdn.digitaloceanspaces.com/logo-short-01.png' },
