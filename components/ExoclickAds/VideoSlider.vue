298004a81a27ea70a2da6dde99cdb574bcac335d
Kuuzou-cl <carlobernucci@gmail.com>
2020-08-18 20:14:26 -0400

    update

--- components/ExoclickAds/VideoSlider.vue
+++ components/ExoclickAds/VideoSlider.vue
@@ -5,9 +5,9 @@
   var adConfig = {
     idzone: 3966060,
     frequency_period: 0,
-    close_after: 0,
+    close_after: 3,
     sound_enabled: 0,
-    on_complete: "repeat",
+    on_complete: "hide",
     branding_enabled: 1,
     screen_density: 25
   };
